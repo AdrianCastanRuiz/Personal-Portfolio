d797a49f45bb424b54725b267ac3de7e16a3d985
adrian <adriancastan23@gmail.com>
2024-12-30 11:32:50 +0000

    feat experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,6 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import winworldLogo from "./winworldlogo.png"
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -64,4 +65,5 @@ export {
   gloton,
   trustify,
   trustifyLogo,
+  winworldLogo
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,7 @@ import {
   meower,
   gloton,
   trustify,
+  winworldLogo
 } from "../assets";
 
 export const navLinks = [
@@ -119,6 +120,18 @@ const experiences = [
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "WinWorld",
+    icon: winworldLogo,
+    iconBg: "#383E56",
+    points: [
+      "Developing and maintaining web applications using React.js, TypeScript, Next JS, Php, SQL and other related technologies.",
+      "Integrated RESTful APIs and third-party services, ensuring seamless data flow and connectivity.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
